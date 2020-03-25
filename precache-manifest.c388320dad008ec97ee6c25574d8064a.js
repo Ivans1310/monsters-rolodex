85a16bc69b9614799bc779b5d817a3fdcb1ef805
Ivan S <ing.ivaneduardosanchez@hotmail.com>
2020-03-25 12:46:14 -0500

    Updates

---
+++ precache-manifest.c388320dad008ec97ee6c25574d8064a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46fd850c947a81e7662d228f03471d60",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "46b6e154840d730c05ce",
+    "url": "/monsters-rolodex/static/css/main.ea53412d.chunk.css"
+  },
+  {
+    "revision": "f09fbcc7ad3b4f9926da",
+    "url": "/monsters-rolodex/static/js/2.1f2f96c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.1f2f96c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46b6e154840d730c05ce",
+    "url": "/monsters-rolodex/static/js/main.d7674374.chunk.js"
+  },
+  {
+    "revision": "420e781ce2f2f4d61faa",
+    "url": "/monsters-rolodex/static/js/runtime-main.9cf5dfda.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
